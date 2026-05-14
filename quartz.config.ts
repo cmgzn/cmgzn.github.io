3f1914bce1e46ef4aa5d56d7f4efebd2f5d19b6d
cmgzn <zdongs@outlook.com>
2026-05-14 14:14:23 +0800

    vault backup: 2026-05-14 14:14:23
    Affected files:
    .obsidian/workspace.json
    quartz.config.ts
    quartz/styles/custom.scss

--- quartz.config.ts
+++ quartz.config.ts
@@ -10,7 +10,7 @@ const config: QuartzConfig = {
   configuration: {
     pageTitle: "Lil b",
     pageTitleSuffix: "",
-    enableSPA: true,
+    enableSPA: false,
     enablePopovers: true,
     analytics: null,
     locale: "zh-CN",
